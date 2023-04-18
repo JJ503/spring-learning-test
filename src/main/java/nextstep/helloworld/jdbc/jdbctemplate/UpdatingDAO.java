7f518fac4ceef0865d5d9e89ea3fb985bf5ec8d5
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-18 11:41:19 +0900

    feat: UpdatingDAO 문제 해결

--- src/main/java/nextstep/helloworld/jdbc/jdbctemplate/UpdatingDAO.java
+++ src/main/java/nextstep/helloworld/jdbc/jdbctemplate/UpdatingDAO.java
@@ -2,8 +2,15 @@ package nextstep.helloworld.jdbc.jdbctemplate;
 
 import nextstep.helloworld.jdbc.Customer;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Repository
 public class UpdatingDAO {
@@ -27,13 +34,16 @@ public class UpdatingDAO {
      */
     public void insert(Customer customer) {
         String sql = "insert into customers (first_name, last_name) values (?, ?)";
+        String firstName = customer.getFirstName();
+        String lastName = customer.getLastName();
+        jdbcTemplate.update(sql, firstName, lastName);
     }
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public int delete(Long id) {
         String sql = "delete from customers where id = ?";
-        return 0;
+        return jdbcTemplate.update(sql, id);
     }
 
     /**
@@ -41,6 +51,16 @@ public class UpdatingDAO {
      */
     public Long insertWithKeyHolder(Customer customer) {
         final String sql = "insert into customers (first_name, last_name) values (?, ?)";
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pstmt = con.prepareStatement(sql, new String[] {"id"});
+                pstmt.setString(1, customer.getFirstName());
+                pstmt.setString(2, customer.getLastName());
+                return pstmt;
+            }
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
     }
 }
